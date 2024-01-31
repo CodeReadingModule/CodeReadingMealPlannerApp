eeb5ffbf7e51e49a49d0edd398471b463b65667f
Matthew Woerner <mwoerner77@gmail.com>
2024-01-30 19:44:36 -0500

    Initial Commit

---
+++ Controllers/MealPlannerController.cs
@@ -0,0 +1,43 @@
+namespace MealPlanner.Controllers
+{
+    using MealPlanner;
+    using MealPlanner.Models;
+    using Microsoft.AspNetCore.Mvc;
+
+    [ApiController]
+    [Route("[controller]")]
+    public class MealPlannerController : ControllerBase
+    {
+        private MealSearcher mealSearcher;
+
+        public MealPlannerController()
+        {
+            //1: 
+            //
+            this.mealSearcher = new MealSearcher();
+        }
+
+        [HttpGet("GetPossibleMeals")]
+        public ActionResult<MealsResponseItem> Get(string mealIngredients)
+        {
+            //2: 
+            //
+            List<string> ingredientList = mealIngredients.Split(',').ToList();
+            MealsResponseItem mealsResponseItem = new MealsResponseItem
+            {
+                MealInformation = this.mealSearcher.GetPossibleMeals(ingredientList)
+            };
+
+            //3: 
+            //
+            if (mealsResponseItem.MealInformation != null && mealsResponseItem.MealInformation.Count > 0) 
+            {
+                return Ok(mealsResponseItem);
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+    }
+}
